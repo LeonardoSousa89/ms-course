2356c711b08799a8af3efe63b69f6c820c7a7c0d
Mendes Sousa <leoprofessionallogytech@outlook.com>
2022-09-07 11:04:48 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/entities/Payment.java
@@ -0,0 +1,51 @@
+package com.devsuperior.hrpayroll.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+public class Payment implements Serializable{
+		private static final long serialVersionUID = 1;
+	
+		private String name;
+		private double dailyIncome;
+		private Integer days;
+		
+		public Payment(){}
+
+		public Payment(String name, double dailyIncome, Integer days) {
+			this.name = name;
+			this.dailyIncome = dailyIncome;
+			this.days = days;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public Integer getDays() {
+			return days;
+		}
+
+		public void setDays(Integer days) {
+			this.days = days;
+		}
+
+		public double getDailyIncome() {
+			return dailyIncome;
+		}
+
+		public void setDailyIncome(double dailyIncome) {
+			this.dailyIncome = dailyIncome;
+		}
+
+		public double getTotal() {
+			return days * dailyIncome;
+		}
+		
+}
